da93e0e303cfff40774ae95a82efb62473a14b96
fiokee <smleffiong@gmail.com>
2024-07-13 17:50:01 -0700

    updating welcome page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 import Logo from '../assets/logo.png';
-// import logo2 from '../assets/Bitcoin-icon.png'
+import logo2 from '../assets/Bitcoin-icon.png'
 
 const NavbarItem = ({ title, classProps }) => {
   return (
@@ -18,7 +18,7 @@ const Navbar = () => {
   return (
     <nav className='w-full flex md:justify-center justify-between items-center p-4'>
       <div className='md:flex-[0.5] flex-initial justify-center items-center'>
-        <img src={Logo} alt='logo' className='w-32 cursor-pointer' />
+        <img src={logo2} alt='logo' className='w-32 cursor-pointer' />
       </div>
       <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
         {["Market", "Exchange", "Tutorials", "Wallet"].map((item, index) => (
